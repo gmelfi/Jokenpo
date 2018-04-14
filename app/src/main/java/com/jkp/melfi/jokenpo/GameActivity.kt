ecb9d6db582a9aa8640adc52cef345eb255700d9
logonrm <logonrm@fiap.com.br>
2018-04-13 21:09:55 -0300

    Initial commit

---
+++ app/src/main/java/com/jkp/melfi/jokenpo/GameActivity.kt
@@ -0,0 +1,89 @@
+package com.jkp.melfi.jokenpo
+
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Random
+import android.media.MediaPlayer
+
+class GameActivity : AppCompatActivity() {
+
+    private var numeroAleatorio: Random? = null
+
+    private val PEDRA = 1
+    private val PAPEL = 1
+    private val TESOURA = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        numeroAleatorio = Random()
+
+        btPedra.setOnClickListener {
+            ivJogadaPlayer!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.m_pedra))
+            realizarJogada(PEDRA)
+        }
+        btPapel.setOnClickListener {
+            ivJogadaPlayer!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.m_pedra))
+            realizarJogada(PAPEL)
+        }
+        btTesoura.setOnClickListener {
+            ivJogadaPlayer!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.m_pedra))
+            realizarJogada(TESOURA)
+        }
+    }
+
+    private fun realizarJogada(jogadaPlayer: Int){
+        //val player = MediaPlayer.create(this.R.raw.jokenpo)
+        //player.start()
+
+        val jogadaPC = numeroAleatorio!!.nextInt(3)+1
+
+        when(jogadaPC){
+            PEDRA ->{
+                ivJogadaPC!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.m_pedra))
+                when(jogadaPlayer){
+                    PAPEL -> venceu()
+                    PEDRA -> empatou()
+                    TESOURA -> perdeu()
+                }
+            }
+
+            PAPEL ->{
+                ivJogadaPC!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.m_papel))
+                when(jogadaPlayer){
+                    PAPEL -> empatou()
+                    PEDRA -> perdeu()
+                    TESOURA -> venceu()
+                }
+            }
+
+            TESOURA ->{
+                ivJogadaPC!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.m_tesoura))
+                when(jogadaPlayer){
+                    PAPEL -> perdeu()
+                    PEDRA -> venceu()
+                    TESOURA -> empatou()
+                }
+            }
+        }
+
+    }
+
+    private fun venceu(){
+        tvResultado!!.text = getString(R.string.venceu)
+        tvResultado!!.setTextColor(ContextCompat.getColor(this, R.color.vitoria))
+    }
+
+    private fun perdeu(){
+        tvResultado!!.text = getString(R.string.perdeu)
+        tvResultado!!.setTextColor(ContextCompat.getColor(this, R.color.derrota))
+    }
+
+    private fun empatou(){
+        tvResultado!!.text = getString(R.string.empatou)
+        tvResultado!!.setTextColor(ContextCompat.getColor(this, R.color.empate))
+    }
+}
